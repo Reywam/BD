86a3985a67c07830c0b22669bb63a7beda937831
Reywam <reywam@mail.ru>
2017-02-10 17:22:25 +0300

    Добавлена первая лабораторная

---
+++ Lab1/Lab1_2/Lab1_2/DataRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab1_2
+{
+    public class DataRow
+    {
+        public string id { get; set; }
+        public string file { get; set; }
+        public string description { get; set; }
+        public string date { get; set; }
+        public string author { get; set; }
+        public string platform { get; set; }
+        public string type { get; set; }
+        public string port { get; set; }
+    }
+}
---
+++ Lab1/Lab1_2/Lab1_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.IO;
+using CsvHelper;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader("db.csv"))
+            {
+                var reader = new CsvReader(streamReader);
+
+                var records = reader.GetRecords<DataRow>().ToList();
+
+                string input = "";
+                while(true)
+                {
+                    Console.Write("Enter username: ");
+                    input = Console.ReadLine();
+
+                    if (input == null)
+                    {
+                        break;
+                    }                    
+
+                    var result = from DataRow record in records
+                                 where record.author.Equals(input)
+                                 select record.id;                    
+
+                    foreach (var index in result)
+                    {
+                        Console.WriteLine(index);
+                    }
+                }
+            }
+        }
+    }
+}
